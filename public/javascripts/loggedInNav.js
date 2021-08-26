9e04ee3ef5a95ba058fe9a78c49d4fbaee5bb005
Katie Young <Katie.f.Young@me.com>
2021-08-26 19:35:44 -0400

    Stuuf

---
+++ public/javascripts/loggedInNav.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", (event) => {
+    // document.querySelectorAll(".nav-link-wrapper")[0].setAttribute("hidden", "true")
+    // document.querySelectorAll(".nav-link-wrapper")[1].setAttribute("hidden", "true")
+    // document.querySelectorAll(".nav-link-wrapper")[2].setAttribute("hidden", "false")
+    //make dynamic
+  })
---
+++ public/javascripts/loggedOutNav.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", (event) => {
+    // document.querySelectorAll(".nav-link-wrapper")[0].setAttribute("hidden", "false")
+    // document.querySelectorAll(".nav-link-wrapper")[1].setAttribute("hidden", "false")
+    // document.querySelectorAll(".nav-link-wrapper")[2].setAttribute("hidden", "true")
+  })
